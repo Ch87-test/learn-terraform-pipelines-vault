774055664754a16b27611aa46d937a998d8f11de
Ch87-test <54280576+Ch87-test@users.noreply.github.com>
2020-11-06 15:59:59 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"
+    organization = "hashicorp-learn-cherrera"
 
     workspaces {
       name = "learn-terraform-pipelines-vault"
